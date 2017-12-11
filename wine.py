46df680643d1b4cf76384c2560cfc55b0b2dedce
Enric Piferrer Torres <epiferre@cs-gpu1.uccs.edu>
2017-12-10 19:56:51 -0700

    adding wine

---
+++ wine.py
@@ -0,0 +1,5 @@
+ead in white wine data 
+white = ___________("http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv", sep=';')
+
+# Read in red wine data 
+red = ___________("http://archive.ics.uci.edu/ml/machine-learning-databases/wine-qualiti/winequalitiddaadfada, sep=';')
