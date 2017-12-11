50d34dc6e5878e335e1492f10981ca824468f5ac
Enric <enric.piferrer@gmail.com>
2017-12-10 20:27:58 -0700

    d

--- wine.py
+++ wine.py
@@ -1,7 +1,19 @@
 import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+
+import numpy as np
+import os.path
+
+
+if not os.path.isfile("../../iris.npy"):
+  pos = np.loadtxt('../../iris.csv', delimiter=',', dtype=np.float32)
+  np.save('../../iris.npy', pos);
+else:
+  pos = np.load('../../iris.npy')
 
 #Read in white wine data
-white = read_csv("iris.csv", sep=';')
+#white = read_csv("iris.csv", sep=';')
 
 # Read in red wine data 
 #red = ___________("http://archive.ics.uci.edu/ml/machine-learning-databases/wine-qualiti/winequalitiddaadfada, sep=';')
